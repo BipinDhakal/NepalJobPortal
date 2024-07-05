747011f87a1e6ff067246ab59d14f3ffc3716695
Bipin Dhakal <bipin.dhakal@dishhome.com.np>
2024-07-05 08:41:26 +0545

    added seed

--- NepalJobPortal/Migrations/20240705025539_seed.Designer.cs
+++ NepalJobPortal/Migrations/20240705025539_seed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NepalJobPortal.Data;
 
@@ -11,9 +12,11 @@ using NepalJobPortal.Data;
 namespace NepalJobPortal.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240705025539_seed")]
+    partial class seed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
